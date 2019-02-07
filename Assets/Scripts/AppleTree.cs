2c5bdfbd86bcc7be14e66f2b71fff39d4df66b8f
cms638 <47014511+cms638@users.noreply.github.com>
2019-02-06 21:58:02 -0500

    Basic Movement

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+    public GameObject applePrefab;
+
+    //speed in which tree moves
+    public float speed = 1f;
+
+    //distance tree moves around
+    public float leftAndRightEdge = 20f;
+
+    //Chance tree change direction
+    public float chancetoChangeDirections = 0.1f;
+
+    //Rate between apples
+    public float secondsBetweenAppleDrops = 1f;
+	
+	void Start () {
+		//dropping apples every second
+	}
+	
+	
+	void Update () {
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Direction
+	}
+}

9a289eb7642019a73ff8baaa4b6a97883da0c811
cms638 <47014511+cms638@users.noreply.github.com>
2019-02-06 23:33:30 -0500

    Move Basket (PROBLEM!!!)

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
-
+    [Header("Set in Inspector")]
     public GameObject applePrefab;
 
     //speed in which tree moves

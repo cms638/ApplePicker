938f3f1f3084f512ec370fded4554b8e461f0a2a
cms638 <47014511+cms638@users.noreply.github.com>
2019-02-07 19:28:46 -0500

    Notes

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -16,15 +16,17 @@ public class Basket : MonoBehaviour {
 
     // Update is called once per frame
 	void Update () {
-
+        // gets x and y from mouse
         Vector3 mousePos2D = Input.mousePosition;
-
+        //gets the z position from main camera
         mousePos2D.z = -Camera.main.transform.position.z;
-
+        //makes it a point within the game space
         Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
-
+        //where am i 
         Vector3 pos = this.transform.position;
+        //where on x axis
         pos.x = mousePos3D.x;
+        //move 
         this.transform.position = pos;
 	}
 
